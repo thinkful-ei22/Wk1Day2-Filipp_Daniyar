aaf4de9b09ab05697c21eaad27232109e29d148f
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-12 16:03:12 -0400

    Completed Turtle exercise

--- Day2Drills.js
+++ Day2Drills.js
@@ -77,7 +77,7 @@ const filteredNames = filter(myNames, function (name) {
   return name[0] === 'R';
 });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
@@ -142,3 +142,24 @@ tornadoWarning('Washington Coast');
 
 floodWarning('Your neighborhood');
 floodWarning('Across the city');
+
+//forEach, filter and map
+
+// A turtle's movements can be represented by an array which looks like this: [3, 4]. The first item in the array represents the number of steps the turtle takes forwards. The second number in the array is the number of steps the turtle takes to the left.
+// Here is an array of different movements made by a turtle: [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]].
+// Use the filter method to remove any items where the turtle moves backwards or to the right(i.e.where either the first of second number is an item that is negative).
+// Use the map method to create a new array containing how many steps the turtle makes in total with each movement(i.e.the first and second number added together).
+// Use the forEach method to log out how many steps the turtle took in each case.
+
+let moves = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+let filteredMoves = moves.filter(arr => {
+  if (arr[0] >= 0 && arr[1] >= 0) { return arr;}
+});
+
+let totalStepsMap = moves.map(move => Math.abs(move[0]) + Math.abs(move [1]));
+
+let totalStepsforEach = totalStepsMap.forEach(num => console.log(num));
+
+console.log(filteredMoves);
+console.log(totalStepsMap);

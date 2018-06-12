1bcd41aefdd898ae2c05a7de98faeb705ed4bda5
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-12 11:30:09 -0700

    Completed Functions as arguments 1

--- Day2Drills.js
+++ Day2Drills.js
@@ -46,3 +46,22 @@ function average (numbers) {
 }
 
 console.log(average(array));
+
+//Functions as arguments(1)
+
+function repeat(fn, n) {
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+}
+
+function hello() {
+  console.log('Hello world');
+}
+
+function goodbye() {
+  console.log('Goodbye world');
+}
+
+console.log(repeat(hello, 5));
+console.log(repeat(goodbye, 5));
